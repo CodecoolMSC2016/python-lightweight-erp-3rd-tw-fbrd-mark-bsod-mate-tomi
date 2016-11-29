1148950a8e6f4bb3d0bd744f73abec56f91ccdfd
Daniel Gal <faramirex070@gmail.com>
2016-11-29 13:53:43 +0100

    add tool_manager_structure_elements to common functions

--- common.py
+++ common.py
@@ -3,6 +3,12 @@
 import random
 
 
+def get_tool_manager_structure_elements():
+    structure_elements = ["id", "name",
+                          "manufacturer", "purchase date", "durability"]
+    return structure_elements
+
+
 # generate and return a unique and random string
 # other expectation:
 # - at least 2 special char()expect: ';'), 2 number, 2 lower and 2 upper case letter
