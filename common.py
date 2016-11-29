982b817e6a65cbd2c76ee30be453d676e32eebe4
Bosnyák Imre Dávid <suti211@gmail.com>
2016-11-29 20:57:36 +0100

    implemented a crude generate_random() func

--- common.py
+++ common.py
@@ -27,9 +27,32 @@ def get_accounting_structure_elements():
 # @table: list of list
 # @generated: string - generated random string (unique in the @table)
 def generate_random(table):
-
     generated = ''
+    ID = 0
 
-    # your code
+    # character sets
+    spec_char = "!@#$%^&*()?"
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    digits = "0123456789"
+
+    present_ids_in_table = []
+
+    # we get all the id-s from the table for checking purposes
+    for entries in table:
+        present_ids_in_table.append(entries[ID])
+
+    # we generate random id-s until we get a unique one which is nor present
+    # in the table, or if we did not generated one already
+    while len(generated) < 8 or generated in present_ids_in_table:
+        generated = ""
+        generated += lowercase[random.randint(0, len(lowercase) - 1)]
+        generated += uppercase[random.randint(0, len(uppercase) - 1)]
+        generated += digits[random.randint(0, len(digits) - 1)]
+        generated += lowercase[random.randint(0, len(lowercase) - 1)]
+        generated += digits[random.randint(0, len(digits) - 1)]
+        generated += spec_char[random.randint(0, len(spec_char) - 1)]
+        generated += spec_char[random.randint(0, len(spec_char) - 1)]
+        generated += uppercase[random.randint(0, len(uppercase) - 1)]
 
     return generated
